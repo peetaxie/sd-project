484cecb08cc88e85b70e88c133559c471b9585dc
peterx <peterx@iastate.edu>
2018-10-01 15:45:21 -0500

    first commit

---
+++ src/components/home.vue
@@ -0,0 +1,29 @@
+<template>
+<GmapMap
+  v-bind:center="{lat:10, lng:10}"
+  v-bind:zoom="7"
+  style="height: 600px"
+>
+  <GmapMarker
+    :key="index"
+    v-for="(m, index) in markers"
+    v-bind:position="m.position"
+    v-bind:clickable="true"
+  />
+</GmapMap>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
